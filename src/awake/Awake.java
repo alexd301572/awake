ec4fa2300eb91ebeeb668ee995cdccde5caffd7a
Alexandru Dragomirescu <alexd301572@gmail.com>
2021-03-22 12:22:28 +0200

    Change infinite loop to scheduled executor
    The while loop has been removed in favor of scheduled executor.

--- src/awake/Awake.java
+++ src/awake/Awake.java
@@ -3,19 +3,29 @@ package awake;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
-import java.lang.Thread;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class Awake {
 
-    public static void main(String[] args) throws AWTException, InterruptedException {
+    public static void main(String[] args) throws AWTException {
         Robot robot = new Robot();
         
-        while (true) {
-            robot.keyPress(KeyEvent.VK_F24);
-            robot.keyRelease(KeyEvent.VK_F24);
-            Thread.sleep(60000);
-        }
+        ScheduledExecutorService scheduler = 
+                Executors.newScheduledThreadPool(1);
         
+        Runnable runCmd = new Runnable() {
+            public void run() {
+                robot.keyPress(KeyEvent.VK_F24);
+                robot.keyRelease(KeyEvent.VK_F24);
+                
+            }  
+        };
+        ScheduledFuture<?> handle = 
+                scheduler.scheduleAtFixedRate(runCmd, 60, 60, TimeUnit.SECONDS);
     }
 
 }
